bd7c0003454b41530455dec07bfc1b9e007d7a39
Nuren7 <nurenislam01@gmail.com>
2026-09-05 21:56:59 +0100

    2

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 const register = async (req, res) => {
-  res.json({message: "IT WORKS"})
-}
+  const body = req.body;
+  res.json(body);
+};
 
-export {register}
+export { register };
--- src/server.js
+++ src/server.js
@@ -1,61 +1,52 @@
 import express from "express";
-import { config } from "dotenv"
+import { config } from "dotenv";
 import { connectDB, disconnectDB } from "./config/db.js";
 
 //Import Routes
 import movieRoutes from "./routes/movieRoutes.js";
-import authRoutes from "./routes/authRoutes.js"
-
+import authRoutes from "./routes/authRoutes.js";
 
 //Load variables
-config()
-connectDB()
-
-
+config();
+connectDB();
 
 const app = express();
 
-
 //API Routes
-app.use("/movies", movieRoutes)
-app.use("/auth", authRoutes)
-
+app.use("/movies", movieRoutes);
+app.use("/auth", authRoutes);
 
 const PORT = 5001;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
 
-
 //Handle unhandled promise rejection eg database connection errors
 process.on("unhandledRejection", (err) => {
-  console.error("Unhandled Rejection:", err)
+  console.error("Unhandled Rejection:", err);
   server.close(async () => {
-    await disconnectDB()
-    process.exit(1)
-  })
-})
-
+    await disconnectDB();
+    process.exit(1);
+  });
+});
 
 //handle uncaught exceptions
 process.on("uncaughtException", (err) => {
-  console.error("Unhandled Exception:", err)
+  console.error("Unhandled Exception:", err);
   server.close(async () => {
-    await disconnectDB()
-    process.exit(1)
-  })
-})
+    await disconnectDB();
+    process.exit(1);
+  });
+});
 
 //Graceful shutdown
 process.on("SIGTERM", async () => {
-  console.error("SIGTERM recieved shutting down gracefully")
+  console.error("SIGTERM recieved shutting down gracefully");
   server.close(async () => {
-    await disconnectDB()
-    process.exit(0)
-  })
-})
-
-
+    await disconnectDB();
+    process.exit(0);
+  });
+});
 
 //GET, POST, PUT, DELETE
 //http://localhost:5001
@@ -63,4 +54,4 @@ process.on("SIGTERM", async () => {
 //AUTH - Signin, Signup
 //MOVIE - GETTING ALL MOVIES
 //USER - Profile
-//WATCHLIST 
+//WATCHLIST
