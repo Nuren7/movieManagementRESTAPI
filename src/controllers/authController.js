168f97486871a512c2dea19f601f7a9990f4b767
Nuren7 <nurenislam01@gmail.com>
2026-09-06 19:43:59 +0100

    Register functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,40 @@
+import { prisma } from "../config/db.js";
+import bcrypt from "bcryptjs";
+
 const register = async (req, res) => {
-  const body = req.body;
-  res.json(body);
+  const { name, email, password } = req.body;
+
+  //Check if user already exist
+
+  const userExists = await prisma.user.findUnique({
+    where: { email: email },
+  });
+
+  if (userExists) {
+    return res
+      .status(400)
+      .json({ error: "User already exists with this email" });
+  }
+
+  //Hash Password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //Create user
+  const user = await prisma.user.create({
+    data: { name, email, password: hashedPassword },
+  });
+
+  res.status(201).json({
+    status: "Success",
+    data: {
+      user: {
+        id: user.id,
+        name: name,
+        email: email,
+      },
+    },
+  });
 };
 
 export { register };
